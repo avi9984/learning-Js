30fbb94fb8f2e9715e3bbaa8e0a6d0d5673d9e96
avi9984 <avinashkumar151199@gmail.com>
2026-07-20 19:41:36 +0530

    reverse trangle pattern

---
+++ Patterns/pattern5.js
@@ -0,0 +1,20 @@
+/**
+ *  Given a value n (positive integer) print the following pattern like this
+ * n=5
+        * * * * *
+        * * * *
+        * * *
+        * *
+        * 
+ */
+
+function pattern(n){
+    for(let row=1; row<=n; row++){
+            let str="";
+        for(let col=1; col<=n-row+1; col++){
+            str+="*";
+        }
+        console.log(str);
+    }
+}
+pattern(5)
