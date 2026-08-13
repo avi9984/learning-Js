f6d0be81c238e3c1a7fe99b4d447ff4b444b8f28
avi9984 <avinashkumar151199@gmail.com>
2026-08-13 19:48:29 +0530

    add reverse array in string

--- reverse_string.js
+++ reverse_string.js
@@ -1,12 +1,22 @@
 // Write a function to reverse the string
 
-function reverse_str(str){
-    let res="";
+function reverse_str(str) {
+    let res = "";
 
-    for(let i=str.length-1; i>=0; i--){
-        res +=str[i];
+    for (let i = str.length - 1; i >= 0; i--) {
+        res += str[i];
     }
     return res;
 }
 console.log(reverse_str("I love JavaScript"));
+
+function reverse_arr(arr) {
+    let res = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        res.push(arr[i])
+    }
+    return res
+}
+
+console.log(reverse_arr([1, 2, 3, 4]));
 
